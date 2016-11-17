67acd3e53692a723258d7e336a687b79063f215d
blairvansant <blairbot@gmail.com>
2016-11-16 20:00:08 -0600

    added firebase to project and imported contact seeder

---
+++ javascripts/appConstants.js
@@ -0,0 +1,9 @@
+"use strict";
+
+app.constant("FIREBASE_CONFIG", {
+	  apiKey: "AIzaSyCEMu_Y2PibISzvzTVp8E3ZgQwMdbt6ylw",
+    authDomain: "angular-contacts-b8a9a.firebaseapp.com",
+    databaseURL: "https://angular-contacts-b8a9a.firebaseio.com",
+    storageBucket: "angular-contacts-b8a9a.appspot.com",
+    messagingSenderId: "973063111316"
+});
